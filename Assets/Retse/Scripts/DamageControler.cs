7d9559f1c5b09ef53e258e13d47ec3f9fe5d2fd1
Retse_dev <stergaldeano@gmail.com>
2024-12-01 20:15:59 +0100

    finished

--- Assets/Retse/Scripts/DamageControler.cs
+++ Assets/Retse/Scripts/DamageControler.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using System.Diagnostics;
 using System.Numerics;
 using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class DamageControler : MonoBehaviour
 {
@@ -19,14 +21,26 @@ public class DamageControler : MonoBehaviour
     private int bulletPosition;
     private float randomPositionX;
     private int randomList;
+    private float spawnVelocity;
 
     void Start()
     {
+        spawnVelocity = 2.0f;
         roundRobinValue = new int[damageList.Length];
-        InvokeRepeating("ChangePosition", 0.0f, 2.0f);
-       
+        //InvokeRepeating("SpawnEnemy", 0.0f, 2.0f);
+        StartCoroutine(Spawn());
     }
 
+    IEnumerator Spawn(){
+        while(true){
+            yield return new WaitForSeconds(spawnVelocity);
+            spawnVelocity = spawnVelocity -0.01f;
+            if (spawnVelocity<0.5){
+                spawnVelocity = 0.5f;
+            }
+            SpawnEnemy();
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -34,7 +48,8 @@ public class DamageControler : MonoBehaviour
         
     }
 
-    public void ChangePosition(){
+    public void SpawnEnemy(){
+
         randomPositionX = Random.Range(leftLimit.gameObject.transform.position.x, rightLimit.gameObject.transform.position.x);
         randomList = Random.Range (0,damageList.Length);
         GameObject damageItem = damageList[randomList];
@@ -45,7 +60,7 @@ public class DamageControler : MonoBehaviour
         roundRobinValue[randomList]++;
 
         if(roundRobinValue[randomList] >= damageItem.gameObject.transform.childCount){
-                roundRobinValue[randomList] = 0;
+            roundRobinValue[randomList] = 0;
         }
         
     }
--- Assets/Retse/Scripts/Player.cs
+++ Assets/Retse/Scripts/Player.cs
@@ -1,12 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private DynamicJoystick dynamicJoystick;
+    private FloatingJoystick floatingJoystick;
     [SerializeField]
     [Range (1,10)]
     private float speed;
+
+     [SerializeField]
+    private GameObject gameManager;
 
     [SerializeField]
     private GameObject explosions;
@@ -14,31 +18,54 @@ public class Player : MonoBehaviour
     [SerializeField]
     private GameObject playerBullets;
 
+    [SerializeField]
+    private GameObject[] sounds;
+
     private int roundRobinValue;
     private int bulletRoundRobinValue;
+     private float shootVelocity;
+  
 
 
     void Start()
     {
+        shootVelocity = 1.0f;
         roundRobinValue = 0;
         bulletRoundRobinValue = 0;
         //InvokeRepeating("shoot",0.0f,1.0f);
+        StartCoroutine(ShootBullet());
     }
 
-    // Update is called once per frame
+
+    IEnumerator ShootBullet(){
+        while(true){
+            yield return new WaitForSeconds(shootVelocity);
+            shootVelocity = shootVelocity -0.005f;
+                if (shootVelocity < 0.3){
+                    shootVelocity = 0.3f;
+            }
+            shoot();
+        }
+    }
+
+ 
+
+
     void Update()
     {
-        this.gameObject.transform.Translate(dynamicJoystick.Horizontal * Time.deltaTime * speed, dynamicJoystick.Vertical * Time.deltaTime * speed, 0.0f);
+        this.gameObject.transform.Translate(floatingJoystick.Horizontal * Time.deltaTime * speed, floatingJoystick.Vertical * Time.deltaTime * speed, 0.0f);
     }
 
     public void shoot(){
 
-        playerBullets.gameObject.transform.GetChild(bulletRoundRobinValue).gameObject.transform.position = this.gameObject.transform.position;
+        sounds[1].gameObject.GetComponent<AudioSource>().Play();
+        GameObject playerBulletChild = playerBullets.gameObject.transform.GetChild(bulletRoundRobinValue).gameObject;
+        playerBulletChild.transform.position = this.gameObject.transform.position;
+        playerBulletChild.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f , 10.0f); 
 
         bulletRoundRobinValue++;  
 
         if(bulletRoundRobinValue >= playerBullets.gameObject.transform.childCount){
-
             bulletRoundRobinValue = 0;
         }
 
@@ -47,15 +74,18 @@ public class Player : MonoBehaviour
     private void hideExplosion(){
 
         for (int i =0 ; i<explosions.gameObject.transform.childCount; i++){
-             explosions.gameObject.transform.GetChild(roundRobinValue).gameObject.SetActive(false);
+             explosions.gameObject.transform.GetChild(i).gameObject.SetActive(false);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other){
 
-        if(other.gameObject.tag != "playerBullets"){
-            explosions.gameObject.transform.GetChild(roundRobinValue).gameObject.transform.position = this.gameObject.transform.position;
-            explosions.gameObject.transform.GetChild(roundRobinValue).gameObject.SetActive(true);
+        if(other.gameObject.tag != "PlayerBullets"){      
+            GameObject explosionChilds = explosions.gameObject.transform.GetChild(roundRobinValue).gameObject;
+
+            //sounds[0].gameObject.GetComponent<AudioSource>().Play(); // sonido cuando golpea al player, usar solo si necesario
+            explosionChilds.transform.position = this.gameObject.transform.position;
+            explosionChilds.SetActive(true);
             Invoke("hideExplosion" , 0.3f);
             roundRobinValue++;
 
@@ -63,9 +93,9 @@ public class Player : MonoBehaviour
                 roundRobinValue = 0;
             }
             
-            //Destroy(other.gameObject);
-             UnityEngine.Debug.Log("Aqui pierdes");
-            // Destroy(this.gameObject);
+            Destroy(other.gameObject);
+            gameManager.gameObject.GetComponent<UIController>().ShowGameOverPanel();
+            Destroy(this.gameObject);
 
         }
       
